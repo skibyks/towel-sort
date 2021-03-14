64847bd5f8490ae0249e4a52b04b56a2fc803f5b
skibyks <skiby_1@mail.ru>
2021-03-14 20:45:51 +0300

    fix: towerl-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
-// You should implement your task here.
+module.exports = function towelSort(matrix){
+  var arr2 = [];
+  if(matrix === undefined){
+    return [];
+  } else {
+    for (i = 1; i < matrix.length; i+=2) {
+  matrix[i].reverse();
+  
+}
+for (i = 0; i < matrix.length; i++){
+  for (j = 0; j < matrix[i].length; j++) {
+    arr2.push(matrix[i][j]);
+  
+  }
+}
+return arr2
+  }
 
-module.exports = function towelSort (matrix) {
-  return  matrix.flat();
 }
